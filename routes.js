866d9f568749b10a82211b4ccfd56ec9559bde95
Sylvan Swierkosz <sswierkosz@gmail.com>
2017-06-19 13:57:06 -0700

    Moves routes to separate file

---
+++ routes.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const database = require('./database')
+
+const router = express()
+
+router.get('/', (request, response) => {
+  database.getAlbums((error, albums) => {
+    if (error) {
+      response.status(500).render('error', { error: error })
+    } else {
+      response.render('index', { albums: albums })
+    }
+  })
+})
+
+router.get('/albums/:albumID', (request, response) => {
+  const albumID = request.params.albumID
+
+  database.getAlbumsByID(albumID, (error, albums) => {
+    if (error) {
+      response.status(500).render('error', { error: error })
+    } else {
+      const album = albums[0]
+      response.render('album', { album: album })
+    }
+  })
+})
+
+router.get('/signup', (request, response) => response.render('sign_up'))
+router.get('/signin', (request, response) => response.render('sign_in'))
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const database = require('./database')
+const router = require('./routes')
 const app = express()
 
 require('ejs')
@@ -9,31 +9,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', (request, response) => {
-  database.getAlbums((error, albums) => {
-    if (error) {
-      response.status(500).render('error', { error: error })
-    } else {
-      response.render('index', { albums: albums })
-    }
-  })
-})
-
-app.get('/albums/:albumID', (request, response) => {
-  const albumID = request.params.albumID
-
-  database.getAlbumsByID(albumID, (error, albums) => {
-    if (error) {
-      response.status(500).render('error', { error: error })
-    } else {
-      const album = albums[0]
-      response.render('album', { album: album })
-    }
-  })
-})
-
-app.get('/signup', (request, response) => response.render('sign_up'))
-app.get('/signin', (request, response) => response.render('sign_in'))
+app.use(router)
 
 app.use((request, response) => {
   response.status(404).render('not_found')
